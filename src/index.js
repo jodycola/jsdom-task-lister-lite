42d7acfda17f0dac08c2b5c1d79de816e603d41a
Kody Samaroo <github email address>
2021-02-08 21:18:14 -0500

    done

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
   
-  const createTasks = document.getElementById('create-task-form')
+  const createTasks = document.querySelector('#create-task-form')
   createTasks.addEventListener('submit', formTask)
 
 
@@ -9,7 +9,7 @@ document.addEventListener("DOMContentLoaded", () => {
       e.preventDefault()
       const newTask = document.querySelector('#new-task-description').value
       const newListItem = document.createElement('li')
-      const tasksList = document.getElementById('tasks')
+      const tasksList = document.querySelector('#tasks')
       newListItem.textContent = newTask
       tasksList.append(newListItem)
   }
